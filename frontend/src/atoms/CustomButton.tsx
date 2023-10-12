200aa0a00a5dd4aff47a5be981cd9496a7ce365e
Mohammed Kaify Iqbal <iqbalmdkaify81@gmail.com>
2023-10-12 20:16:06 +0530

    removed fixed font style for button text

--- frontend/src/atoms/CustomButton.tsx
+++ frontend/src/atoms/CustomButton.tsx
@@ -6,7 +6,7 @@ export type CustomButtonProps = {
 
 const CustomButton = ({ text, customStyles, icon }: CustomButtonProps) => {
     return (
-        <button className={`flex gap-2 justify-center items-center px-6 py-2 font-semibold hover:text-[#4e4feb] ${customStyles} transition duration-300`}>
+        <button className={`flex gap-2 justify-center items-center px-6 py-2 hover:text-[#4e4feb] ${customStyles} transition duration-300`}>
             {icon}
             {text}
         </button>
