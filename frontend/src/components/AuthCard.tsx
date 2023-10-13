c5aaf4fab29d54188816ea98bbafcd383f881de6
Mohammed Kaify Iqbal <iqbalmdkaify81@gmail.com>
2023-10-13 12:51:12 +0530

    saving pre-changes before implementing react-router

--- frontend/src/components/AuthCard.tsx
+++ frontend/src/components/AuthCard.tsx
@@ -2,13 +2,52 @@ import { DiDjango } from 'react-icons/di';
 import { FaReact } from 'react-icons/fa';
 import { SiReactrouter } from 'react-icons/si';
 import { PiWebhooksLogo } from 'react-icons/pi';
-// import RegisterForm from '../atoms/RegisterForm';
 import LoginForm from '../atoms/LoginForm';
 
 const AuthCard = () => {
+
     return (
-        <div>
-            Lorem
+        // main wrapper div which will take the full space of body--> w,h-screen
+        <div className='w-full h-full md:p-[70px] text-[#141E46]'>
+            <div className='grid md:grid-cols-2 items-center justify-center'>
+                {/* login div container */}
+                <div className='flex justify-center items-center'>
+                    {/* login form */}
+                    <LoginForm />
+                </div>
+
+                {/* content container */}
+                <div className='flex flex-col items-center justify-between h-[calc(100vh-140px)] px-6 md:px-0'>
+                    <div className='w-full flex flex-col justify-between items-center text-center'>
+                        {/* texts */}
+                        <div className='flex flex-col items-center justify-between gap-[3rem]'>
+                            <h1 className='font-semibold md:text-[4.768rem] text-[3.052rem]'>
+                                Test your code and db.
+                            </h1>
+                            <h4 className='font-normal text-center md:text-[2.441rem] text-[1.953rem]'>
+                                Explore the power of React hooks and Contexts with this website along with django.
+                            </h4>
+                            <h2 className='font-semibold text-[#C70039] text-[2.441rem] md:text-[3.815rem]'>
+                                Powered by
+                            </h2>
+                        </div>
+
+                        {/* logos of partners, lmao */}
+                        <div className='flex gap-[60px] mt-[2rem]'>
+                            <DiDjango size={70} color="green" />
+                            <SiReactrouter size={70} color="red" />
+                            <PiWebhooksLogo size={70} />
+                            <FaReact size={70} color="#61dbfb" />
+                        </div>
+                    </div>
+
+                    {/* rem texts */}
+                    <div className='text-base text-center'>
+                        <p className='text-[#C70039] underline hover:cursor-pointer'>No thanks, I’m good</p>
+                        <p className='text-[#141E46]'>This link takes you to nowhere, you don’t have any options</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { useState } from 'react';
+import { Outlet, NavLink } from 'react-router-dom'
 
 import CustomButton from "../atoms/CustomButton"
 
@@ -11,40 +12,45 @@ const NavBar = () => {
     }
 
     return (
-        <nav className='w-full h-[4rem] text-white bg-black flex justify-between items-center px-6'>
-            <h1 className='font-semibold text-xl md:text-3xl'>TEST APP</h1>
+        <>
+            <nav className='w-full h-[5rem] text-white bg-black flex justify-between items-center px-6'>
+                <h3 className='font-semibold text-[2.441rem] lg:text-[3.052rem]'>Codedb</h3>
 
-            {/* Desktop Navigation */}
-            <div className='hidden md:flex items-center justify-between gap-20'>
-                <ul className='flex space-x-10 items-center'>
-                    <li>Home</li>
-                    <li>Explore</li>
-                    <li>Contact</li>
-                    <CustomButton
-                        text="LOGOUT"
-                        customStyles="hover:outline hover:outline-[1px] hover:outline-[#4e4feb]"
-                    />
-                </ul>
-                <p>Welcome, User</p>
-            </div>
-            <div onClick={handleNavVisibility} className='block md:hidden'>
-                {visible ? <AiOutlineClose size={20} color="#4e4feb" /> : <AiOutlineMenu size={20} color="#4e4feb" />}
-            </div>
-
-            {/* Mobile Navigation */}
-            <ul className={visible ? "fixed top-0 left-0 mt-[4rem] text-[#4e4feb] h-[calc(100%-4rem)] flex flex-col bg-[#100F0F] w-full justify-center items-center gap-[4rem] text-xl py-8 ease-in duration-300" : " fixed top-0 left-[-100%] mt-[4rem] h-[calc(100%-4rem)]"}>
-                <li>Home</li>
-                <li>Explore</li>
-                <li>Contact</li>
-                <div className='flex flex-col space-y-4 items-center'>
-                    <CustomButton
-                        text="LOGOUT"
-                        customStyles="hover:outline hover:outline-[1px] hover:outline-[#4e4feb] text-white"
-                    />
-                    <p className='text-white'>Welcome, User</p>
+                {/* Desktop Navigation */}
+                <div className='hidden md:flex items-center justify-between gap-20'>
+                    <ul className='flex space-x-10 items-center'>
+                        <NavLink className='text-[1.25rem]' to="/">Home</NavLink>
+                        <NavLink className='text-[1.25rem]' to="explore">Explore</NavLink>
+                        <NavLink className='text-[1.25rem]' to="contact">Contact</NavLink>
+                        <CustomButton
+                            text="LOGOUT"
+                            customStyles="hover:outline hover:outline-[1px] hover:outline-[#4e4feb]"
+                        />
+                    </ul>
+                    <p className='text-[1.25rem]'>Welcome, iqbalmd81</p>
                 </div>
-            </ul>
-        </nav>
+                <div onClick={handleNavVisibility} className='block md:hidden'>
+                    {visible ? <AiOutlineClose size={20} color="#4e4feb" /> : <AiOutlineMenu size={20} color="#4e4feb" />}
+                </div>
+
+                {/* Mobile Navigation */}
+                <ul className={visible ? "fixed top-0 left-0 mt-[5rem] text-[#4e4feb] h-[calc(100%-4rem)] flex flex-col bg-[#100F0F] w-full justify-center items-center gap-[5rem] text-xl py-8 ease-in duration-300" : " fixed top-0 left-[-100%] mt-[4rem] h-[calc(100%-4rem)]"}>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="explore">Explore</NavLink>
+                    <NavLink to="contact">Contact</NavLink>
+                    <div className='flex flex-col space-y-4 items-center'>
+                        <CustomButton
+                            text="LOGOUT"
+                            customStyles="hover:outline hover:outline-[1px] hover:outline-[#4e4feb] text-white font-bold"
+                        />
+                        <p className='text-white'>Welcome, User</p>
+                    </div>
+                </ul>
+            </nav>
+            <main>
+                <Outlet />
+            </main>
+        </>
     )
 }
 
